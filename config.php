21b409bcacbb8111f578053e777bd21e75257346
mareeo <dman99@gmail.com>
2016-05-23 01:28:20 -0500

    Config file made more general

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'vendor/php-activerecord/ActiveRecord.php';
-require_once 'vendor/functions.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -11,7 +10,7 @@ ActiveRecord\Config::initialize(function($cfg)
 {
    $cfg->set_model_directory('src/Forum/Models');
    $cfg->set_connections(array('production' =>
-   'mysql://root:root@localhost/deepgame_forum?charset=utf8mb4'));
+   'mysql://DBUSER:DBPASS@DBHOST/DBNAME?charset=utf8mb4'));
      
    # default connection is now production
    $cfg->set_default_connection('production');
@@ -22,7 +21,7 @@ ActiveRecord\Config::initialize(function($cfg)
 date_default_timezone_set('America/Chicago');
 mb_internal_encoding('UTF-8');
 
-define('WEB_BASE_DIR',      '/dustinhibbard.com/forum/');
+define('WEB_BASE_DIR',      '/forum/');
 define('FS_BASE_DIR',       __DIR__ . '/');
 define('THUMB_MAX_WIDTH',   200);
 define('THUMB_MAX_HEIGHT',  200);
@@ -34,9 +33,9 @@ define('FS_THUMB_DIR',  FS_IMG_DIR . 't/');
 
 define('ADMIN_PASSWORD', 'adminPassword');
 
-$pdo = new PDO('mysql:host=localhost;dbname=deepgame_forum;charset=utf8mb4', "root", "root");
+$pdo = new PDO('mysql:host=DBHOST;dbname=DBNAME;charset=utf8mb4', "DBUSER", "DBPASS");
 
 $FORUM = new stdClass;
-$FORUM->version = 0.92;
-$FORUM->date = '4/17/2016';
+$FORUM->version = 1.00;
+$FORUM->date = '5/23/2016';
 $FORUM->author = "Dustin Hibbard <dustinhibbard@gmail.com>";
